1851495d65e4cd83c22cfd45aaa09e5d033a21a6
Faisal-Khan-Tushar <faisal15-13400@diu.edu.bd>
2021-10-08 13:47:11 +0600

    added a form

--- src/App.js
+++ src/App.js
@@ -15,8 +15,29 @@ function App() {
    console.log(user);
  })
   }
+  //amader register button e click marle page ta reload martese. Eita input type er submit button e default behaviour. Behaviour take stop korte chaile amader ke ekta parameter nite hobe function tate 'e' name ar pore likhte hobe je e.preventDefault(); tahole ar page ta reload marbe na. 
+  const handleRegistration=e=>{
+ console.log('ergistration will be added');
+ e.preventDefault();
+  }
   return (
     <div className="App">
+{/* submit type er input thakle amra form er moddhe ekta event handler add korte pari sheta hocche onSubmit={} jeta kina submit button e click korle kichu ekta je ghotbe oita control korbe */}
+  <form onSubmit={handleRegistration} >
+    <h3>Please Register</h3>
+    {/* htmlFor = kisher jonno ami label ta banacchi oita eitar vitore likha lage */}
+    <label htmlFor="email">Email:</label>
+    <input type="text" name="email" />
+    <br/>
+    <label htmlFor="password">Password</label>
+    <input type="password" name="password" id="" />
+    <br/>
+    {/* submit er jonno input type amra submit nei */}
+    <input type="submit" value="Register"  />
+  </form>
+ <br /><br /><br />
+      <div>------------------------------------</div>
+      <br /><br /><br />
       <button onClick={handleGoogleSignIn}>Google sign in</button>
     </div>
   );
